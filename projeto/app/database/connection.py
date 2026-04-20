29b6dd802949429089c90b7d58b625a9d028a7f7
Vinicius <hubner.vini@gmail.com>
2026-04-20 19:49:53 -0300

    Ajustando a  conexão do banco pro Render

--- projeto/app/database/connection.py
+++ projeto/app/database/connection.py
@@ -1,13 +1,14 @@
+import os
 import psycopg
-from app.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise ValueError("DATABASE_URL não está definida")
+
     return psycopg.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        autocommit=False,
+        database_url,
+        autocommit=False
     )
